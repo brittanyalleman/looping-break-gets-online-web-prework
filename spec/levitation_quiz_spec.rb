43ad8a5b2bb094c05c22acac8111d74eb703de1b
Brittany Alleman <brittany.alleman1@gmail.com>
2019-05-08 19:45:15 +0000

    Automatically backed up by Learn

--- spec/levitation_quiz_spec.rb
+++ spec/levitation_quiz_spec.rb
@@ -2,6 +2,6 @@ require_relative "spec_helper"
 
 describe "#levitation_quiz" do
   puts "What is the spell that enacts levitation?"
-  break if  == "Wingardium Leviosa"
+  break if == "Wingardium Leviosa"
  end
   puts "you passed the quiz!"
